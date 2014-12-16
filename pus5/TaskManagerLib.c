7b336374910e302ae631a4ab713d6b8a40c3e5c3
Michał Daniel <michal.p.daniel@gmail.com>
2014-12-16 21:46:47 +0100

    Update TaskManagerLib.c

--- pus5/TaskManagerLib.c
+++ pus5/TaskManagerLib.c
@@ -9,6 +9,7 @@
 static Task tasks[MAX_NUMBER_OF_TASKS];
 
 void schedule(){
+	cli();
 	for(int i = 0; i<MAX_NUMBER_OF_TASKS; i++){
 		tasks[i].current++;
 		if(tasks[i].current == tasks[i].period){
@@ -16,20 +17,32 @@ void schedule(){
 			tasks[i].current = 0;
 		}
 	}
+	sei();
 };
 
 void execute(){
-	int position = 0;
 	while(true){
-		if(tasks[position].ready == true){
-			tasks[position].ready = false;
-			tasks[position].func_ptr(tasks[position].params);
+		cli();
+		for(int position=0;position<MAX_NUMBER_OF_TASKS;position++){
+			if(tasks[position].ready == true){
+				tasks[position].ready = false;
+				sei();
+				tasks[position].func_ptr(tasks[position].params);
+				position=0;
+				}else{
+				break;
+			}
 		}
-		position++;
-		if(position>MAX_NUMBER_OF_TASKS){
-			position = 0;
-		}
+		sei();
 	}
+};
+
+void AddTask(int priority, int period, TaskPointer func_ptr, void* params){
+	tasks[priority].period = period;
+	tasks[priority].current = 0;
+	tasks[priority].ready = false;
+	tasks[priority].func_ptr = func_ptr;
+	tasks[priority].params = params;
 };
 
 void AddTask(int priority, int period, TaskPointer func_ptr, void* params){
